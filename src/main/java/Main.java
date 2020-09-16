302b9f526107a90d9f9a55be8c23eb0d9f1fc7a5
Pavel <54819066+Fischerski1@users.noreply.github.com>
2020-09-16 22:49:24 +0300

    + delete commented str

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        /*GenerateObjects generateObjects = new GenerateObjects();
-        String[] array = generateObjects.generatePersons();
-
-        for (int i = 0; i < array.length; i++) {
-            System.out.println(array[i]);
-        }*/
 
         BubbleSorted bubbleSorted = new BubbleSorted();
-        //bubbleSorted.sort();
 
         Person[] array = bubbleSorted.sort();
 
